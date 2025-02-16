7d9cbe354108f12abac0e4433352272fa7bac197
North-West-Wind <wsyn148@gmail.com>
2025-02-16 15:07:00 +0800

    tap timeout retry

--- get-token.sh
+++ get-token.sh
@@ -31,9 +31,24 @@ $ADB_DIR/adb shell monkey -p com.nintendo.znca 1 > /dev/null
 echo "Launched NSO app"
 
 # keep trying to tap if no gtoken is found
+tries=0
 while ! tail -n2 $mitmout | grep -qe "gToken"; do
 	sleep 1
 	$ADB_DIR/adb shell input tap 270 1200
+	((tries=tries+1))
+	# timeout at 60 seconds
+	if [ $tries -gt 60 ]; then
+		echo "Timed out trying to get gToken. Closing app and retrying..."
+		# close app
+		$ADB_DIR/adb shell am force-stop com.nintendo.znca
+		echo "Closed app"
+		sleep 2
+		# re-launch app
+		$ADB_DIR/adb shell monkey -p com.nintendo.znca 1 > /dev/null
+		echo "Launched NSO app"
+		# reset counter
+		tries=0
+	fi
 done
 echo "Tapped into SplatNet 3"
 
